95f158847dffd2b26cb39af29cbcffd435f51c42
ttirtia <ttirtia@vulyn.com>
2019-01-02 07:54:05 +0000

    Update dependencies

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
 
   // and load the index.html of the app.
