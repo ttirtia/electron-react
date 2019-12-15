bdfada8f2883842e3105665482d8eefa74b8d77d
Thibaut Tirtia <ttirtia@vulyn.com>
2019-12-15 15:50:35 +0100

    Update dependencies and fix lint warnings

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const path = require('path');
 const electron = require('electron');
 
 // Module to control application life.
-const app = electron.app;
+const { app } = electron;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
+const { BrowserWindow } = electron;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -31,7 +31,7 @@ function createWindow() {
   );
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function closed() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -45,7 +45,7 @@ function createWindow() {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function windowAllClosed() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -53,7 +53,7 @@ app.on('window-all-closed', function() {
   }
 });
 
-app.on('activate', function() {
+app.on('activate', function activate() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
